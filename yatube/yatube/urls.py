a5874acb4cbe44ca8c2836215640d143b2178a59
AgarkovEgor <agaregor46@icloud.com>
2023-03-08 18:24:30 +0300

    done

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('posts.urls')),
@@ -10,6 +11,8 @@ urlpatterns = [
     path('about/', include('about.urls', namespace='about')),
     path('post/', include('posts.urls', namespace='posts'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'core.views.page_not_found'
 handler403 = 'core.views.csrf_failure'
